d741da40900a2ef99aa3b3ddc70d10866307c45f
Dhruv <dhruvmishra375@gmail.com>
2022-11-22 00:19:01 +0530

    issues with crypto detail page and settings screen

--- lib/crypto_detail.dart
+++ lib/crypto_detail.dart
@@ -96,11 +96,4 @@ class _CryptoDetailCardState extends State<CryptoDetailCard> {
     );
   }
 
-  String _parseHtmlString(String htmlString) {
-  final document = parse(htmlString);
-  final String parsedString = parse(document.body!.text).documentElement!.text;
 
-  return parsedString;
-  }
-
-}
